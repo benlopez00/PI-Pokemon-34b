53cdbe488b88fb795a2e80fb8de993923c0d0621
BenLopez00 <lop.ben00@gmail.com>
2023-02-27 12:37:08 -0300

    Rutas que faltaban

--- PI-Pokemon-main/api/src/routes/RutaPoke.js
+++ PI-Pokemon-main/api/src/routes/RutaPoke.js
@@ -50,5 +50,111 @@ router.get('/', async (req, res) => {
         console.log("here be error", err.message);
         res.send({ msg: err.message });
     }
-
 })
+
+router.post('/create', async (req, res) => {
+    const {
+        name, hp, attack, defense, speed, height, weight, image
+    } = req.body;
+    if (!name || !hp || !attack || !defense || !speed || !height || !weight) {
+        return res.status(400).json({
+            info: `Theres a missing value`
+        })
+    }
+    let arrType = []
+    req.body.types.map(e => arrType.push({ name: e }))
+    if (!arrType.length) { return res.status(400).json({ info: `Choose at least one type` }) }
+
+    const exists = await Pokemon.findOne({ where: { name: req.body.name } })
+
+    if (exists) return res.json({ info: "This pokemons already exists!" });
+    try {
+        const newPoke = await Pokemon.create({
+            name: req.body.name,
+            hp: req.body.hp,
+            attack: req.body.attack,
+            defense: req.body.defense,
+            speed: req.body.speed,
+            height: req.body.height,
+            weight: req.body.weight,
+            createdDb: true,
+            img: image ? image : "https://media.giphy.com/media/DRfu7BT8ZK1uo/giphy.gif"
+        });
+        let typeDb = await Types.findAll({ where: { name: arrType[0].name } })
+        newPoke.addType(typeDb);
+        if (arrType[1]) {
+            let typeDb2 = await Types.findAll({ where: { name: arrType[1].name } })
+            newPoke.addType(typeDb2);
+        }
+
+        res.status(201).send({ msg: 'Pokemon created successfully!' })
+    } catch (err) {
+        console.log(err);
+    }
+
+
+});
+
+router.put('/modify/:id', async (req, res) => {
+    let { id } = req.params;
+    let { name, hp, attack, defense, speed, height, weight, img } = req.body;
+    try {
+        let find = await Pokemon.findOne({ where: { id: id } })
+        if (find) {
+
+            await Pokemon.update({
+                name: name ? name : find.name,
+                hp: hp ? hp : find.hp,
+                attack: attack ? attack : find.attack,
+                defense: defense ? defense : find.defense,
+                speed: speed ? speed : find.speed,
+                height: height ? height : find.height,
+                weight: weight ? weight : find.weight,
+                img: img ? img : find.img
+            }, { where: { id: id } })
+
+            return res.send({ msg: "Updated successfully" });
+        }
+        res.send({ msg: "This Pokemon doen't exist" });
+
+    } catch (err) {
+        console.log("This be the message", err.message)
+        res.send({ msg: err.message })
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        let { id } = req.params;
+        const findIt = await Pokemon.findOne({
+            where: { idPoke: id },
+            include: {
+                model: Types,
+                attributes: ['name'],
+                through: { attributes: [], }
+            }
+        });
+        res.send(findIt);
+    } catch (err) {
+        console.log(err.message);
+        res.send({ msg: err.msg });
+    }
+});
+
+router.delete('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const toDelete = await Pokemon.findOne({ where: { id: id } })
+        if (toDelete) {
+            await Pokemon.destroy({ where: { id: id } })
+            return res.send({ msg: "Pokemon Deleted successfully" });
+        }
+        res.send({ msg: `The Pokemon id  ${id}  doesn't exist ` });
+    } catch (err) {
+        console.log({ msg: err.message })
+        res.send({ msg: err.message })
+    }
+});
+
+
+module.exports = router;
--- PI-Pokemon-main/api/src/routes/RutaTipo.js
+++ PI-Pokemon-main/api/src/routes/RutaTipo.js
@@ -10,3 +10,23 @@ router.get('/', async (req, res) => {
         res.send({ msg: err.message });
     }
 })
+
+router.post('/', async (req, res) => {
+    const { name } = req.body;
+    try {
+        let exists = await Types.findOne({ where: { name: name } });
+        if (!exists) {
+            await Types.create({
+                name: name
+            });
+            return res.send({ msg: "You just created a new Type of pokemon!" })
+        }
+        res.send({ msg: "This type of pokemon already exists" });
+    } catch (err) {
+        console.log('here be the error', err.message);
+        res.send({ msg: err.message });
+    }
+})
+
+
+module.exports = router;
