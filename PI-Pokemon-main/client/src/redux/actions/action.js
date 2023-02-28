fc0b72d764413f31125cd2b8fcbc886401408077
BenLopez00 <lop.ben00@gmail.com>
2023-02-27 21:32:56 -0300

    Redux terminado

--- PI-Pokemon-main/client/src/redux/actions/action.js
+++ PI-Pokemon-main/client/src/redux/actions/action.js
@@ -14,3 +14,70 @@ export function fetchPokemons() {
         }
     }
 }
+
+export function fetchTypes() {
+    return async function (dispatch) {
+        try {
+            const fetchedTypes = await axios(`${BASE_URL}/type`);
+            return dispatch({
+                type: "GET_TYPES",
+                payload: fetchedTypes.data
+            })
+        } catch (err) {
+            console.log({ msg: err.message })
+        }
+    }
+}
+
+export function findByName(name) {
+    return async function (dispatch) {
+        try {
+            const pokeName = await axios(`${BASE_URL}/pokemon?name=${name}`)
+            return dispatch({
+                type: "FIND_BY_NAME",
+                payload: pokeName.data
+            })
+        } catch (err) {
+            console.log({ msg: err.message })
+        }
+    }
+}
+
+export function postPokemon(payload) {
+    return async function (dispatch) {
+        const response = await axios.post(`${BASE_URL}/pokemon/create`, payload);
+        return dispatch({
+            type: "CREATE_POKEMON",
+            response
+        })
+    }
+}
+
+export function fetchDetails(id) {
+    return async function (dispatch) {
+        const details = await axios(`${BASE_URL}/pokemon/${id}`);
+        return dispatch({
+            type: "GET_DETAILS",
+            payload: details.data
+        })
+    }
+}
+
+
+export function filterByCreated(payload) {
+    return {
+        type: "FILTER_BY_CREATED",
+        payload
+    }
+}
+
+export function filterPokemons(payload) {
+    return async function (dispatch) {
+        const filters = await axios.get(`${BASE_URL}/pokemon?typeFilter=${payload.typeFilter}&order=${payload.order}`)
+
+        return dispatch({
+            type: "FILTERS",
+            payload: filters.data
+        })
+    }
+}
