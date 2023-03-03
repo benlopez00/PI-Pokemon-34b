9d039f33ceed657d0cdd93d37e61f3a22f048757
BenLopez00 <lop.ben00@gmail.com>
2023-03-02 23:39:10 -0300

    Home con navbar, form y search

---
+++ PI-Pokemon-main/client/src/components/Form/index.jsx
@@ -0,0 +1,261 @@
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { postPokemon, fetchTypes } from "../../redux/actions/actionTypes";
+import { useDispatch, useSelector } from "react-redux";
+import TypeIcon from "../TypeIcon/TypeIcon";
+import './index.css'
+
+export default function Form() {
+    const dispatch = useDispatch();
+    const types = useSelector((state) => state.types);
+    const history = useHistory();
+
+    const [input, setInput] = useState({
+        name: "",
+        hp: "",
+        attack: "",
+        defense: "",
+        speed: "",
+        height: "",
+        weight: "",
+        image: "",
+        types: [],
+    });
+    const [errors, setErrors] = useState("");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        if (Object.values(errors).length) {
+            let message = ''
+            console.log(message)
+            let err = Object.values(errors)
+            return alert(message = err.map(e => e + '\n'))
+        } else {
+            const { name, hp, attack, defense, speed, image, height, weight, types } = input;
+            if (name && hp && attack && defense && speed && height && weight && types.length !== 0) {
+                dispatch(postPokemon(input));
+
+                alert('Pokemon created successfully!')
+            } else alert("Some field is missing information");
+            setInput({
+                name: "",
+                hp: "",
+                attack: "",
+                defense: "",
+                speed: "",
+                height: "",
+                weight: "",
+                image: "",
+                types: [],
+            });
+            history.push("/pokemons");
+        }
+    }
+
+    useEffect(() => {
+        dispatch(fetchTypes());
+    }, [dispatch]);
+
+    function handleChange(e) {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value,
+        });
+
+        setErrors(
+            validate({
+                ...input,
+                [e.target.name]: [e.target.value],
+            })
+        );
+
+    }
+
+    function handleClearTypes(event) {
+        setInput({
+            ...input,
+            types: []
+        })
+    }
+
+    function handleSelect(event) {
+        setInput({
+            ...input,
+            types: [...input.types, event.target.value],
+        });
+    }
+    function tisNumber(n) {
+        if (/^\d+$/.test(n)) {
+            return true;
+        }
+        return false;
+    }
+
+    function tisString(n) {
+        if (/^\D+$/.test(n)) {
+            return true;
+        }
+        return false;
+    }
+
+    function validate(input) {
+        let errors = {};
+        if (!input.name) {
+            errors.name = "Please input a name";
+
+        } else if (!tisString(input.name)) errors.name = "characters only";
+
+        else if (!input.hp) errors.hp = "missing value";
+
+        else if (!tisNumber(input.hp)) errors.hp = "numbers only";
+
+        else if (!input.attack) errors.attack = "missing value";
+
+        else if (!tisNumber(input.attack)) errors.attack = "numbers only";
+
+        else if (!input.defense) errors.defense = "missing value";
+
+        else if (!tisNumber(input.defense)) errors.defense = "numbers only";
+
+        else if (!input.speed) errors.speed = "missing value";
+
+        else if (!tisNumber(input.speed)) errors.speed = "numbers only";
+
+        else if (!input.weight) errors.weight = "missing value";
+
+        else if (!tisNumber(input.weight)) errors.weight = "numbers only";
+
+        else if (!input.height) errors.height = "missing value";
+
+        else if (!tisNumber(input.height)) errors.height = "numbers only";
+        // if (input.types.length === 0) errors.types = "You must choose a Type for your pokemon!!"
+        return errors;
+    }
+    return (
+        <div className="form-container">
+            {/* <p className="form-tittle">Create your pokemon!</p> */}
+            <form
+                onSubmit={(e) => handleSubmit(e)}
+                className='form-layout' >
+                <div >
+                    <input
+                        className='generic-input'
+                        required
+                        type="text"
+                        placeholder="Name"
+                        name="name"
+                        value={input.name}
+                        onChange={(e) => handleChange(e)}
+                    />
+                    {errors.name && <p className="errors">{errors.name}</p>}
+                </div>
+
+                <div>
+                    <input
+                        className='generic-input'
+                        type="number"
+                        placeholder="Health"
+                        name="hp"
+                        value={input.hp}
+                        onChange={(e) => handleChange(e)}
+                    />
+                    {errors.hp && <p className="errors">{errors.hp}</p>}
+                </div>
+
+                <div>
+                    <input
+                        className='generic-input'
+                        type="number"
+                        placeholder="Attack"
+                        name="attack"
+                        value={input.attack}
+                        onChange={(e) => handleChange(e)}
+                    />
+                    {errors.attack && <p className="errors">{errors.attack}</p>}
+                </div>
+
+                <div>
+                    <input
+                        className='generic-input'
+                        type="number"
+                        placeholder="Defense"
+                        name="defense"
+                        value={input.defense}
+                        onChange={(e) => handleChange(e)}
+                    />
+                    {errors.defense && <p className="errors">{errors.defense}</p>}
+                </div>
+
+                <div>
+                    <input
+                        className='generic-input'
+                        type="number"
+                        placeholder="Speed"
+                        name="speed"
+                        value={input.speed}
+                        onChange={(e) => handleChange(e)}
+                    />
+                    {errors.speed && <p className="errors">{errors.speed}</p>}
+                </div>
+
+                <div>
+                    <input
+                        className='generic-input'
+                        type="number"
+                        placeholder="Height"
+                        name="height"
+                        value={input.height}
+                        onChange={(e) => handleChange(e)}
+                    />
+                    {errors.height && <p className="errors">{errors.height}</p>}
+                </div>
+                <div>
+                    <input
+                        className='generic-input'
+                        type="number"
+                        placeholder="Weight"
+                        name="weight"
+                        value={input.weight}
+                        onChange={(e) => handleChange(e)}
+                    />
+                    <div>
+                        {errors.weight && <p className="errors">{errors.weight}</p>}
+
+                    </div>
+                </div>
+                <input
+                    className="generic-input"
+                    type="text"
+                    name="image"
+                    placeholder={("pick an image")}
+                    onChange={(e) => handleChange(e)}
+                    value={input.image}
+                />
+                <select
+                    className='generic-input'
+                    onChange={(e) => {
+                        handleSelect(e);
+                    }}
+                >
+                    {types.map((e) => {
+                        return (
+                            <option value={e.name} key={e.id}>
+                                {e.name}
+                            </option>
+                        );
+                    })}
+                </select>
+
+
+
+                <div className="form-types">
+                    {input.types.map((e) => (<TypeIcon name={e} />))}
+                </div>
+                <div className="button-container">
+                    <button className="realodButton" href={null} onClick={handleClearTypes} >Clear</button>
+                    <button className="realodButton" type="submit" >Create!</button>
+                </div>
+            </form>
+        </div>
+    );
+}
---
+++ PI-Pokemon-main/client/src/components/SearchBar/index.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { findByName } from "../../redux/actions/actionTypes";
+import './styles.css'
+export default function SearchBar() {
+    const allPokes = useSelector((state) => state.pokemons);
+    const dispatch = useDispatch();
+    const [name, setName] = useState("");
+    const [sugestions, setSugestion] = useState([]);
+
+
+    function handleInputChange(name) {
+        let matches = []
+        console.log("here be name", typeof name)
+        if (name.length > 0) {
+            matches = allPokes.filter(pokemon => {
+                const regex = new RegExp(`${name}`, "gi");
+                return pokemon.name.match(regex);
+            }).splice(0, 3)
+        }
+        setSugestion(matches)
+        setName(name);
+    }
+
+    function sugestionHandler(name) {
+        setName(name);
+        setSugestion([])
+        dispatch(findByName(name))
+    }
+
+    function handleSubmit() {
+        dispatch(findByName(name));
+    }
+
+    return (
+        <div className='searchBar-container'>
+            <div className="searchSugestion">
+                <input
+                    // className='generic-input'
+                    type="text"
+                    placeholder="Search.."
+                    onChange={(e) => handleInputChange(e.target.value)}
+                    onBlur={() => {
+                        setTimeout(() => setSugestion([]), 200)
+                    }}
+                />
+                {sugestions?.length ? (
+                    <div className="sugestion-div">
+                        <ul className="sugestion-ul">
+                            {sugestions.map((sugestion, i) =>
+                                <li
+                                    className="sugestion-li"
+                                    key={i}
+                                    onClick={() => sugestionHandler(sugestion.name)}>{sugestion.name}
+                                </li>)}
+                        </ul>
+                    </div>
+                ) : null}
+            </div>
+            <div>
+                <button className='realodButton' type="submit" onClick={(e) => handleSubmit(e.target.value)}>
+                    Search
+                </button>
+            </div>
+        </div>
+    );
+}
